02591fdc62c25d991086b0dc9319e059b184dd8c
amberlowe1001 <52145666+amberlowe1001@users.noreply.github.com>
2021-03-01 12:37:02 -0800

    first commit

--- largest = none.py
+++ largest = none.py
@@ -1,9 +1,10 @@
-import
 
-largest: int = int
+largest: None = None
 print("Before:", largest)
 for interval in [3, 41, 12, 9, 74, 15]:
-    if largest is None or largest < interval:
+    if largest is not None and largest >= interval:
+        pass
+    else:
         largest = interval
     print('Loop:', interval, largest)
 print('Largest:', largest)
