300df1ee4dfdff9e05bcf82c9c90be2d70347eee
amberlowe1001 <52145666+amberlowe1001@users.noreply.github.com>
2021-03-04 19:49:35 -0800

    last commit

---
+++ lists/lists.py
@@ -0,0 +1,22 @@
+# app.py
+
+str = 'AppDividend'
+print(type(str))
+
+int = 123
+print(type(int))
+
+float = 21.19
+print(type(float))
+
+negative = -19
+print(type(negative))
+
+dictionary = {'blog':'AppDividend'}
+print(type(dictionary))
+
+list = [1, 2, 3]
+print(type(list))
+
+tuple = (19, 21, 46)
+print(type(tuple))
